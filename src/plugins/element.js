4300fedf04bce69f26b57add55701408cd50d942
yi <279729918@qq.com>
2020-06-11 00:27:25 +0800

    登录界面完成

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Form, FormItem, Input, message } from 'element-ui'
+
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.prototype.$message = message
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+import Login from '../components/Login'
+import Home from '../components/Home'
 Vue.use(VueRouter)
 
 const routes = [{
-
+        path: '/',
+        redirect: '/login' //路由重定向
+    },
+    {
+        path: '/login',
+        component: Login
+    },
+    {
+        path: '/home',
+        component: Home
     }
-
 ]
 
 const router = new VueRouter({
     routes
 })
 
+//挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+    if (to.path === '/login') return next();
+    const tokenStr = window.sessionStorage.getItem('token');
+    if (!tokenStr) return next('/login');
+    next()
+
+})
 export default router
