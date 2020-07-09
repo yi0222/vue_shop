a36f2c185a31c6b083baac2c1567b3883ff8a750
yi <279729918@qq.com>
2020-07-09 17:05:20 +0800

    完成商品功能开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -31,7 +31,12 @@ import {
     Cascader,
     Alert,
     Tabs,
-    TabPane
+    TabPane,
+    Steps,
+    Step,
+    CheckboxGroup,
+    Checkbox,
+    Upload
 } from 'element-ui'
 
 
@@ -65,5 +70,10 @@ Vue.use(Cascader)
 Vue.use(Alert)
 Vue.use(Tabs)
 Vue.use(TabPane)
+Vue.use(Steps)
+Vue.use(Step)
+Vue.use(Checkbox)
+Vue.use(CheckboxGroup)
+Vue.use(Upload)
 Vue.prototype.$message = message
 Vue.prototype.$confirm = MessageBox.confirm
